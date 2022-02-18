d977b5d2b107bb2524440270aaeb606d0f3561db
Nandhini Vijayakumar <nandhinivijayakumar@nandhinis-air.esuhsd.org>
2022-02-17 17:09:58 -0800

    Vision working but not complete

--- opencv-i-think/detectFlagV2.py
+++ opencv-i-think/detectFlagV2.py
@@ -6,49 +6,76 @@ import numpy as np
 capture = cv.VideoCapture(0)
 
 while True:
-    isTrue, img = capture.read() 
+    isTrue, img = capture.read()               
 
-    hsvImg = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    # hsvImg = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    # lower_color_bound = np.array([30,30,30])
+    # upper_color_bound = np.array([255, 255, 255])
 
-    lower_color_bound = np.array([30,30,30])
-    upper_color_bound = np.array([255, 255, 255])
+
+    # mask = cv.inRange(hsvImg, lower_color_bound, upper_color_bound)
+    # inverted_image = cv.bitwise_not(mask)
 
 
-    mask = cv.inRange(hsvImg, lower_color_bound, upper_color_bound)
-    inverted_image = cv.bitwise_not(mask)
+    # img3 = cv.cvtColor(inverted_image, cv.COLOR_HSV2RGB)
+    img2 = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
 
-    img2 = inverted_image.copy()
-    template = cv.imread('opencv-i-think/photos/matching2.jpg',0)
-    w, h = template.shape[::-1]
-    # All the 6 methods for comparison in a list
-    methods = 'cv.TM_CCOEFF'
-    img = img2.copy()
-    method = eval(methods)
-    # Apply template Matching
-    res = cv.matchTemplate(img,template,method)
+    templates = [cv.imread('opencv-i-think/photos/68.jpg', 0), cv.imread('opencv-i-think/photos/115.jpg', 0), cv.imread('opencv-i-think/photos/162.jpg', 0), cv.imread('opencv-i-think/photos/209.jpg', 0), cv.imread('opencv-i-think/photos/256.jpg', 0), cv.imread('opencv-i-think/photos/303.jpg', 0)]
+    
+    
+    ret, img3 = cv.threshold(img2, 253, 255, 0)
+    cv.imshow("img", img3)
+    cnt = 1
+    
+
+
+
+
+
+    ####
+
+    highestMatch = 0
+
+    for i in range(len(templates)):
+        template = templates[i]         
+        method = cv.TM_CCOEFF_NORMED  
+            # Apply template Matching
+        temp_res = cv.matchTemplate(img3, template, method) # this is problem line pls help or else the continuing suffering will comence 
+        if np.amax(temp_res) >= highestMatch:
+            w, h = template.shape[::-1] 
+            highestMatch = np.amax(temp_res)
+            res = temp_res
+        # else:
+        #     res = cv.matchTemplate(img3, template, method)
+        #print(str(cnt) + " " + str(np.amax(temp_res)))
+        cnt = cnt+1
+
+ 
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
     top_left = max_loc
     bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv.rectangle(img,top_left, bottom_right, 255, 2)
+    if (np.amax(highestMatch) < 0.7) and (np.amax(highestMatch) > 0.39):
+        cv.rectangle(img3,top_left, bottom_right, 255, 2)
 
-    imgH, imgW = img.shape
+    imgH, imgW = img3.shape
     calibZeroPixel = int(0.76 * imgH)
 
     rectTop = top_left[1]
-    recBottom = bottom_right[1]
+    rectBottom = bottom_right[1]
 
 
 
-    detectedHeight = int((rectTop+recBottom)/2)
+    detectedHeight = int((rectTop+rectBottom)/2)
 
 
-    cv.line(img, (0, calibZeroPixel), (imgW, calibZeroPixel), color=255, thickness=3)
-    cv.line(img, (0, detectedHeight), (imgW, detectedHeight), color=255, thickness=3)
-    print(imgW)
-    cv.imshow('img',img)
+    #cv.line(img, (0, calibZeroPixel), (imgW, calibZeroPixel), color=255, thickness=3)
+    #cv.line(img, (0, detectedHeight), (imgW, detectedHeight), color=255, thickness=3)
+
+    print(np.amax(highestMatch))
+    cv.imshow('img',img3)
 
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
 capture.release()
 cv.destroyAllWindows()
+
